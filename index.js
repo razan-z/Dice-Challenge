9748731647cbfb8ac8b9c810586f6aa57a00a5ae
Razan <113352191+razan-z@users.noreply.github.com>
2024-07-30 18:55:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var dice1 = Math.floor(Math.random() * 6) + 1;
+var dice2 = Math.floor(Math.random() * 6) + 1;
+
+document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + dice1 + ".png");
+document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + dice2 + ".png");
+
+var h1 = "";
+if (dice1 > dice2) {
+    h1 += "🚩 Player 1 Wins";
+}
+else if (dice2 > dice1) {
+    h1 += "Player 2 Wins 🚩";
+}
+else {
+    h1 += "Draw!"
+}
+document.querySelector("h1").textContent = h1;
